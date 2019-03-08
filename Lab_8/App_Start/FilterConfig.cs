b115e15d72d9ec7c4cf3e9bf04253ae11cd4c7ff
Matt Rondini <mrondini12@bw.edu>
2019-03-07 23:16:17 -0500

    Add project files.

---
+++ Lab_8/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_8
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new AuthorizeAttribute());
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
